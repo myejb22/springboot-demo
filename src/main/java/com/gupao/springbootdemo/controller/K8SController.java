8b27b951d523ef7014488eddb536ce5a85678ec5
myejb22 <myejb22@163.com>
2019-12-17 22:42:45 +0000

    merge version

--- src/main/java/com/gupao/springbootdemo/controller/K8SController.java
+++ src/main/java/com/gupao/springbootdemo/controller/K8SController.java
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-package com.gupao.springbootdemo.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.net.InetAddress;
-
-@RestController
-public class K8SController {
-
-
-    @RequestMapping("/k8s")
-    public String k8s(){
-        return "hello K8s <br/>111222 ";
-    }
-}
-=======
 package com.gupao.springbootdemo.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,4 +14,3 @@ public class K8SController {
         return "hello K8s Jack";
     }
 }
->>>>>>> d49db651f156bb6ae4537a388c818502675d76d5
