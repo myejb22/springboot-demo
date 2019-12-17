4c6d1e233af76123eaeb421ac446b8bb7e358236
myejb22 <myejb22@163.com>
2019-12-17 13:49:50 +0000

    first my spring boot

---
+++ src/main/java/com/gupao/springbootdemo/controller/K8SController.java
@@ -0,0 +1,35 @@
+<<<<<<< HEAD
+package com.gupao.springbootdemo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+
+@RestController
+public class K8SController {
+
+
+    @RequestMapping("/k8s")
+    public String k8s(){
+        return "hello K8s <br/>111222 ";
+    }
+}
+=======
+package com.gupao.springbootdemo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+
+@RestController
+public class K8SController {
+
+
+    @RequestMapping("/k8s")
+    public String k8s(){
+        return "hello K8s Jack";
+    }
+}
+>>>>>>> d49db651f156bb6ae4537a388c818502675d76d5
